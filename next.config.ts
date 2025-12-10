ceb763f000afd909723d086d695fe803e8afb9c5
fredtare <tarenomm@tlu.ee>
2025-12-10 22:30:32 +0200

    refactor: prepare to deploy to gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',              // enable static export (`next export`)
+  images: {
+    unoptimized: true,           // GitHub Pages can't do Next image optimization
+  },
+    typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // If your site will be at https://<username>.github.io/<repo>
+  basePath: '/rakenduste-programmeerimine-2025/ehitus-kontaktihaldur',
+  assetPrefix: '/rakenduste-programmeerimine-2025/ehitus-kontaktihaldur/',
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
